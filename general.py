0eff806374ec878213627b07c7a3fdb0ca8159f0
emcaruso <emcaruso@unibz.it>
2023-08-03 15:33:56 +0200

    argparse

--- general.py
+++ general.py
@@ -1,3 +1,6 @@
+import json
+import argparse
+
 def print_hash(string, offs=5):
     l = len(string)+ (offs+1)*2
     print("\n"+"#" * l)
@@ -5,5 +8,14 @@ def print_hash(string, offs=5):
     print("#" * l+"\n")
 
 
+def dump_parsed_args( path, args ):
+    with open( path, 'w') as f:
+        json.dump(args.__dict__, f, indent=2)
 
+def load_parsed_args( path):
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    with open(path, 'r') as f:
+        args.__dict__ = json.load(f)
+    return args
 
