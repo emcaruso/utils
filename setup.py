9b6ed8c29a4e15f81a199a549b182c6cb9764ec8
emanuelecaruso <caruso.manu81@gmail.com>
2023-08-06 13:28:39 +0200

    utils install

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
 from setuptools import setup, find_packages
  
-setup(name = "utils_ema", packages = find_packages())
+setup(  name = "utils_ema",
+        packages=find_packages(include=['utils_ema','utils_ema.*']),
+
+        version="1.0")
