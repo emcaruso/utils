cd11f4e753eb702618746349ad548651135dc6a8
emcaruso <emcaruso@unibz.it>
2023-08-29 19:27:50 +0200

    chk

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
  
 setup(  name = "utils_ema",
-        packages=find_packages(include=['utils_ema','utils_ema.*']),
-
+        packages=find_packages(include=['utils_ema/']),
         version="1.0")
---
+++ utils_ema/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+ 
+setup(  name = "utils_ema",
+        # packages=find_packages(include=['utils_ema','utils_ema.*']),
+        packages=find_packages(),
+
+        version="1.0")
